c9e11fc0e7f7ef7bc7818ddf08eb2f23d736fb00
rakeshramesh98 <rakesh98raji@gmail.com>
2021-02-12 11:38:49 +0530

    Calendar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './index1.scss';
+import React from 'react';
 function App() {
+  
+  const day = ['sun','mon','tue','wen','thu','fri','sat'];
+  function ListItem(props) {    ///display days row
+    const value = props.value;
+    const i=props.index
+    const colPos=i+1
+      return (
+        <div className={`ms-grid-col${colPos} ms-grid-row2`}>
+          {value}
+        </div>
+      );
+  }
+  const day2= (new Array(42)).fill(null).map((v,i)=>(v,i+1))
+
+  function ListItem2(props) {     ////display date rows
+    const value2 = props.value2;
+    const rowPos=2+Math.ceil(value2/7) //1,2,3,4,...7
+    const colPos=value2-((rowPos-3)*7) //1,2,3,4...7,1,2,3..7,1,2....7
+    return (
+      <div className={`ms-grid-col${colPos} ms-grid-row${rowPos}`}>
+        {value2}
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="Calendar">
+      <button className="ms-grid-col1 ms-grid-row1">Prev</button>
+      <h2 class="ms-grid-col2 ms-grid-row1 title">February</h2>  
+      <button className="ms-grid-col7 ms-grid-row1">Next</button>
+
+              {day.map((day,i) =>
+        <ListItem value={day} index={i}/> 
+      )}
+              {day2.map((day2) =>
+        <ListItem2 value2={day2} />
+      )}
     </div>
   );
 }
